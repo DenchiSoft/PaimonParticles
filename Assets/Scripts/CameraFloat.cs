320d9494c074e2d526cc2f64b57ab9f95387a945
Denchi <denchisoft@gmail.com>
2020-11-21 03:10:00 +0100

    Working demo

---
+++ Assets/Scripts/CameraFloat.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class CameraFloat : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float BaseSpeed;
+
+    private float leftRightSpeed;
+    private float upDownSpeed;
+    private float slowdownFactor = 0.91f;
+
+    void FixedUpdate()
+    {
+        checkKey(KeyCode.LeftArrow, KeyCode.A, ref leftRightSpeed, BaseSpeed);
+        checkKey(KeyCode.RightArrow, KeyCode.D, ref leftRightSpeed, -BaseSpeed);
+        checkKey(KeyCode.UpArrow, KeyCode.W, ref upDownSpeed, BaseSpeed);
+        checkKey(KeyCode.DownArrow, KeyCode.S, ref upDownSpeed, -BaseSpeed);
+
+        upDownSpeed *= slowdownFactor;
+        leftRightSpeed *= slowdownFactor;
+
+        upDownSpeed = Mathf.Abs(upDownSpeed) > 0.001f ? upDownSpeed : 0;
+        leftRightSpeed = Mathf.Abs(leftRightSpeed) > 0.001f ? leftRightSpeed : 0;
+
+        var angles = transform.eulerAngles;
+        angles.x += upDownSpeed * 0.5f;
+        angles.y += leftRightSpeed * 0.5f;
+        transform.eulerAngles = angles;
+    }
+
+    private void checkKey(KeyCode key1, KeyCode key2, ref float value, float add)
+    {
+        value += (Input.GetKey(key1) || Input.GetKey(key2)) ? add : 0;
+    }
+}
---
+++ Assets/Scripts/PaimonParticles.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// Stripped down version of the "Plexus" effect by Mirza Beig (MIT License).
+/// See https://www.youtube.com/watch?v=ruNPkuYT1Ck
+/// </summary>
+public class PaimonParticles : MonoBehaviour
+{
+    // Renderer to use for the lines between particles.
+    public LineRenderer lineRendererTemplate;
+
+    private ParticleSystem lineParticleSystem;
+    private ParticleSystem.Particle[] particles;
+    private ParticleSystem.MainModule particleSystemMainModule;
+    private List<LineRenderer> lineRenderers = new List<LineRenderer>();
+
+    // Particle properties.
+    private Vector3[] particlePositions;
+    private Color[] particleColors;
+    private float[] particleSizes;
+
+    void Start()
+    {
+        lineParticleSystem = GetComponent<ParticleSystem>();
+        particleSystemMainModule = lineParticleSystem.main;
+
+        int maxParticles = particleSystemMainModule.maxParticles;
+
+        if (particles == null || particles.Length < maxParticles)
+        {
+            particles = new ParticleSystem.Particle[maxParticles];
+            particlePositions = new Vector3[maxParticles];
+            particleColors = new Color[maxParticles];
+            particleSizes = new float[maxParticles];
+        }
+    }
+
+    void OnDisable()
+    {
+        lineRenderers.ForEach(renderer => renderer.enabled = false);
+    }
+
+    void LateUpdate()
+    {
+        int lineRenderersCount = lineRenderers.Count;
+
+        Color lineRendererStartColor = lineRendererTemplate.startColor;
+        Color lineRendererEndColor = lineRendererTemplate.endColor;
+
+        float lineRendererStartWidth = lineRendererTemplate.startWidth * lineRendererTemplate.widthMultiplier;
+        float lineRendererEndWidth = lineRendererTemplate.endWidth * lineRendererTemplate.widthMultiplier;
+
+        fillParticlePropertyCache();
+
+        int lrIndex = 0;
+
+        for (int i = 0; i < lineParticleSystem.particleCount; i++)
+        {
+            Color particleColor = particleColors[i];
+            Color lineStartColor = particleColor;
+
+            float lineStartColorOriginalAlpha = Mathf.LerpUnclamped(lineRendererStartColor.a, particleColor.a, 0.95f);
+
+            lineStartColor.a = lineStartColorOriginalAlpha;
+
+            float lineStartWidth = Mathf.LerpUnclamped(lineRendererStartWidth, particleSizes[i], 0.15f);
+
+            if (i + 1 < lineParticleSystem.particleCount)
+            {
+                LineRenderer lr;
+
+                if (lrIndex == lineRenderersCount)
+                {
+                    lr = Instantiate(lineRendererTemplate, transform, false);
+                    lineRenderers.Add(lr);
+                    lineRenderersCount++;
+                }
+
+                lr = lineRenderers[lrIndex];
+
+                lr.enabled = true;
+                lr.SetPosition(0, particlePositions[i]);
+                lr.SetPosition(1, particlePositions[i + 1]);
+                lineStartColor.a = lineStartColorOriginalAlpha;
+                lr.startColor = lineStartColor;
+
+                particleColor = particleColors[i + 1];
+
+                Color lineEndColour = particleColor;
+                lineEndColour.a = Mathf.LerpUnclamped(lineRendererEndColor.a, particleColor.a, 1);
+            
+                lr.endColor = lineEndColour;
+                lr.startWidth = lineStartWidth;
+                lr.endWidth = Mathf.LerpUnclamped(lineRendererEndWidth, particleSizes[i + 1], 0.15f);
+
+                lrIndex++;
+            }
+        }
+
+        for (int i = lrIndex; i < lineRenderersCount; i++)
+        {
+            if (lineRenderers[i].enabled)
+            {
+                lineRenderers[i].enabled = false;
+            }
+        }
+    }
+
+    private void fillParticlePropertyCache()
+    {
+        lineParticleSystem.GetParticles(particles);
+
+        for (int i = 0; i < lineParticleSystem.particleCount; i++)
+        {
+            particlePositions[i] = particles[i].position;
+            particleColors[i] = particles[i].GetCurrentColor(lineParticleSystem);
+            particleSizes[i] = particles[i].GetCurrentSize(lineParticleSystem);
+        }
+    }
+}
